4aaf1f505743e889637e59f352875fc5ca16421e
david <david@vlijmincx.com>
2025-01-07 12:22:28 +0100

    Added mixed read write test

--- src/test/java/com/davidvlijmincx/lio/api/JLibUringBlockingTest.java
+++ src/test/java/com/davidvlijmincx/lio/api/JLibUringBlockingTest.java
@@ -40,6 +40,29 @@ class JLibUringBlockingTest {
     }
 
     @Test
+    void multiReadFromFile() {
+        BlockingReadResult result = jLibUringBlocking.prepareRead("src/test/resources/read_file", 14, 0);
+        BlockingReadResult result1 = jLibUringBlocking.prepareRead("src/test/resources/read_file", 5, 0);
+        BlockingReadResult result2 = jLibUringBlocking.prepareRead("src/test/resources/read_file", 7, 7);
+
+        jLibUringBlocking.submit();
+
+        // make it valid UTF-8
+        result.getBuffer().set(JAVA_BYTE, result.getResult(), (byte) 0);
+        result1.getBuffer().set(JAVA_BYTE, result1.getResult(), (byte) 0);
+        result2.getBuffer().set(JAVA_BYTE, result2.getResult(), (byte) 0);
+
+        assertEquals("Hello, World!", result.getBuffer().getString(0));
+        assertEquals("Hello", result1.getBuffer().getString(0));
+        assertEquals("World!", result2.getBuffer().getString(0));
+
+        jLibUringBlocking.freeReadBuffer(result.getBuffer());
+        jLibUringBlocking.freeReadBuffer(result1.getBuffer());
+        jLibUringBlocking.freeReadBuffer(result2.getBuffer());
+
+    }
+
+    @Test
     void readFromFileAtOffset() {
         BlockingReadResult result = jLibUringBlocking.prepareRead("src/test/resources/read_file", 6, 7);
 
@@ -85,5 +108,33 @@ class JLibUringBlockingTest {
         String writtenContent = Files.readString(path);
         assertEquals("Big hello, from Java", writtenContent);
 
+    }
+
+    @Test
+    void mixedWriteAndReadFromFile() {
+        Path writePath = Path.of("src/test/resources/write_file");
+        Path readPath = Path.of("src/test/resources/read_file");
+
+        String input = "hello, from Java";
+        var inputBytes = input.getBytes();
+
+        BlockingReadResult readResult = jLibUringBlocking.prepareRead(readPath.toString(), 14, 0);
+        BlockingWriteResult writeResult = jLibUringBlocking.prepareWrite(writePath.toString(), inputBytes, 4);
+        BlockingReadResult readResult1 = jLibUringBlocking.prepareRead(readPath.toString(), 5, 0);
+        BlockingWriteResult writeResult1 = jLibUringBlocking.prepareWrite(writePath.toString(), inputBytes, 4);
+        BlockingReadResult readResult2 = jLibUringBlocking.prepareRead(readPath.toString(), 7, 7);
+
+        jLibUringBlocking.submit();
+
+        assertEquals(13, readResult.getResult());
+        assertEquals(5, readResult1.getResult());
+        assertEquals(6, readResult2.getResult());
+
+        assertEquals(16, writeResult.getResult());
+        assertEquals(16, writeResult1.getResult());
+
+        jLibUringBlocking.freeReadBuffer(readResult.getBuffer());
+        jLibUringBlocking.freeReadBuffer(readResult1.getBuffer());
+        jLibUringBlocking.freeReadBuffer(readResult2.getBuffer());
     }
 }
