840f0d4c81da3e43b9620cd8411acd0c114df625
David Vlijmincx <David@davidvlijmincx.com>
2025-06-22 20:04:45 +0000

    Add missing methods to blocking API

--- src/main/java/com/davidvlijmincx/lio/api/JUringBlocking.java
+++ src/main/java/com/davidvlijmincx/lio/api/JUringBlocking.java
@@ -1,11 +1,13 @@
 package com.davidvlijmincx.lio.api;
 
 
+import java.lang.foreign.MemorySegment;
 import java.time.Duration;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Future;
+import java.util.function.Supplier;
 
 public class JUringBlocking implements AutoCloseable {
 
@@ -55,18 +57,71 @@ public class JUringBlocking implements AutoCloseable {
         jUring.submit();
     }
 
-    public Future<ReadResult> prepareRead(FileDescriptor fd, int size, long offset) {
-        long id = jUring.prepareRead(fd, size, offset);
-        CompletableFuture<ReadResult> result = new CompletableFuture<>();
+    private <T extends Result> Future<T> prepareAsync(Supplier<Long> prepareOperation) {
+        long id = prepareOperation.get();
+        CompletableFuture<T> result = new CompletableFuture<>();
         requests.put(id, result);
         return result;
     }
 
+    public Future<ReadResult> prepareRead(FileDescriptor fd, int size, long offset) {
+        return prepareAsync(() -> jUring.prepareRead(fd, size, offset));
+    }
+
     public Future<WriteResult> prepareWrite(FileDescriptor fd, byte[] bytes, long offset) {
-        long id = jUring.prepareWrite(fd, bytes, offset);
-        CompletableFuture<WriteResult> result = new CompletableFuture<>();
-        requests.put(id, result);
-        return result;
+        return prepareAsync(() -> jUring.prepareWrite(fd, bytes, offset));
+    }
+
+    public Future<ReadResult> prepareRead(int indexFD, int readSize, long offset, SqeOptions... sqeOptions) {
+        return prepareAsync(() -> jUring.prepareRead(indexFD, readSize, offset, sqeOptions));
+    }
+
+    public Future<ReadResult> prepareReadFixed(FileDescriptor fd, int readSize, long offset, int bufferIndex, SqeOptions... sqeOptions) {
+        return prepareAsync(() -> jUring.prepareReadFixed(fd, readSize, offset, bufferIndex, sqeOptions));
+    }
+
+    public Future<ReadResult> prepareReadFixed(int indexFD, int readSize, long offset, int bufferIndex, SqeOptions... sqeOptions) {
+        return prepareAsync(() -> jUring.prepareReadFixed(indexFD, readSize, offset, bufferIndex, sqeOptions));
+    }
+
+    public Future<WriteResult> prepareWrite(int indexFD, byte[] bytes, long offset, SqeOptions... sqeOptions) {
+        return prepareAsync(() -> jUring.prepareWrite(indexFD, bytes, offset, sqeOptions));
+    }
+
+    public Future<WriteResult> prepareWriteFixed(FileDescriptor fd, byte[] bytes, long offset, int bufferIndex, SqeOptions... sqeOptions) {
+        return prepareAsync(() -> jUring.prepareWriteFixed(fd, bytes, offset, bufferIndex, sqeOptions));
+    }
+
+    public Future<WriteResult> prepareWriteFixed(int indexFD, byte[] bytes, long offset, int bufferIndex, SqeOptions... sqeOptions) {
+        return prepareAsync(() -> jUring.prepareWriteFixed(indexFD, bytes, offset, bufferIndex, sqeOptions));
+    }
+
+    public Future<OpenResult> prepareOpen(String filePath, int flags, int mode, SqeOptions... sqeOptions) {
+        return prepareAsync(() -> jUring.prepareOpen(filePath, flags, mode, sqeOptions));
+    }
+
+    public Future<OpenResult> prepareOpenDirect(String filePath, int flags, int mode, int fileIndex, SqeOptions... sqeOptions) {
+        return prepareAsync(() -> jUring.prepareOpenDirect(filePath, flags, mode, fileIndex, sqeOptions));
+    }
+
+    public Future<CloseResult> prepareClose(FileDescriptor fd, SqeOptions... sqeOptions) {
+        return prepareAsync(() -> jUring.prepareClose(fd, sqeOptions));
+    }
+
+    public Future<CloseResult> prepareCloseDirect(int fileIndex, SqeOptions... sqeOptions) {
+        return prepareAsync(() -> jUring.prepareCloseDirect(fileIndex, sqeOptions));
+    }
+
+    public MemorySegment[] registerBuffers(int size, int nrOfBuffers) {
+        return jUring.registerBuffers(size, nrOfBuffers);
+    }
+
+    public int registerFiles(FileDescriptor... fileDescriptors) {
+        return jUring.registerFiles(fileDescriptors);
+    }
+
+    public int registerFilesUpdate(int offset, int[] fileDescriptors) {
+        return jUring.registerFilesUpdate(offset, fileDescriptors);
     }
 
     @Override
