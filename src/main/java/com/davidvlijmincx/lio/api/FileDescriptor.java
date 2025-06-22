dcef7a4b88d9b3cb1755c5a809d9d1a33fc298a2
David Vlijmincx <David@davidvlijmincx.com>
2025-06-22 18:46:13 +0000

    Moved close checks to client side

--- src/main/java/com/davidvlijmincx/lio/api/FileDescriptor.java
+++ src/main/java/com/davidvlijmincx/lio/api/FileDescriptor.java
@@ -3,7 +3,6 @@ package com.davidvlijmincx.lio.api;
 public class FileDescriptor implements AutoCloseable {
 
     private final int fd;
-    private boolean closed = false;
 
     public FileDescriptor(String path, LinuxOpenOptions flags, int mode) {
         this.fd = NativeDispatcher.C.open(path, flags.getValue(), mode);
@@ -14,18 +13,12 @@ public class FileDescriptor implements AutoCloseable {
     }
 
     int getFd() {
-        if (closed) {
-            throw new IllegalStateException("File descriptor has been closed");
-        }
         return fd;
     }
 
     @Override
     public void close() {
-        if (!closed) {
-            NativeDispatcher.C.close(fd);
-            closed = true;
-        }
+        NativeDispatcher.C.close(fd);
     }
 
 }
